459d6ba44e8fc87c1856b1fdb0d1b996c39813f0
y0n1 <y0n1@outlook.com>
2024-07-30 09:02:50 +0000

    adds the skipList
    Signed-off-by: y0n1 <y0n1@outlook.com>

--- packages/json/_deep_map_keys_test.ts
+++ packages/json/_deep_map_keys_test.ts
@@ -6,77 +6,111 @@ import {
 } from "jsr:@std/assert";
 import camelCase from "npm:lodash/camelCase.js";
 import { deepMapKeys } from "./deep_map_keys.ts";
+import type { JsonArray } from "./types.ts";
 
-Deno.test("json.ts/deepMapKeys", async (t) => {
-  await t.step("should throw if there is a circular reference", () => {
-    const a = { prop: {} };
-    a.prop = a;
+Deno.test("deepMapKeys", async (t) => {
+  await t.step({
+    ignore: false,
+    name: "should throw if there is a circular reference",
+    fn: () => {
+      const a = { prop: {} };
+      a.prop = a;
 
-    try {
-      assertThrows(() => deepMapKeys(a), TypeError);
-    } catch (error) {
-      assert(error.message, "Converting circular structure");
-    }
+      try {
+        assertThrows(() => deepMapKeys(a), TypeError);
+      } catch (error) {
+        assert(error.message, "Converting circular structure");
+      }
+    },
   });
 
-  await t.step("should throw if the passed value is not a JSON scalar", () => {
-    try {
-      assertThrows(() => deepMapKeys(new Set()), TypeError);
-    } catch (error) {
-      assert(error.message, "Invalid argument type received");
-    }
+  await t.step({
+    ignore: false,
+    name: "should throw if the passed value is not a JSON scalar",
+    fn: () => {
+      try {
+        assertThrows(() => deepMapKeys(new Set()), TypeError);
+      } catch (error) {
+        assert(error.message, "Invalid argument type received");
+      }
+    },
   });
 
-  await t.step(
-    "should return a new empty object when an empty object is passed",
-    () => {
+  await t.step({
+    ignore: false,
+    name: "should return a new empty object when an empty object is passed",
+    fn: () => {
       const expected = {};
+
       const actual = deepMapKeys({});
 
       assertEquals(actual, expected);
       assertFalse(actual === expected);
     },
-  );
+  });
 
-  await t.step(
-    "should return the same value when the given value is a JSON scalar",
-    () => {
+  await t.step({
+    ignore: false,
+    name: "should return a new empty array when an empty array is passed",
+    fn: () => {
+      const expected: JsonArray = [];
+
+      const actual = deepMapKeys(expected, camelCase);
+
+      assertEquals(actual, expected);
+      assertFalse(actual === expected);
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name: "should return the same value when the given value is a JSON scalar",
+    fn: () => {
       const scalars = [null, false, true, "some_string", 42];
 
       for (const v of scalars) {
         assertEquals(deepMapKeys(v, camelCase), v);
       }
     },
-  );
-
-  await t.step("should transform all the keys in the passed value", () => {
-    const jsonObject = {
-      first_name: "John",
-      last_name: "Doe",
-      address: { street_name: "Main St" },
-    };
-
-    const result = deepMapKeys(jsonObject, camelCase);
-
-    assertEquals(result, {
-      firstName: "John",
-      lastName: "Doe",
-      address: {
-        streetName: "Main St",
-      },
-    });
   });
 
-  await t.step("should discard keys with undefined values", () => {
-    const expected = {};
-    const actual = deepMapKeys({ prop: undefined });
+  await t.step({
+    ignore: false,
+    name: "should transform all the keys in the passed value",
+    fn: () => {
+      const jsonObject = {
+        first_name: "John",
+        last_name: "Doe",
+        address: { street_name: "Main St" },
+      };
 
-    assertEquals(actual, expected);
+      const result = deepMapKeys(jsonObject, camelCase);
+
+      assertEquals(result, {
+        firstName: "John",
+        lastName: "Doe",
+        address: {
+          streetName: "Main St",
+        },
+      });
+    },
   });
 
-  await t.step(
-    "should transform the keys of nested objects in JsonArrays",
-    () => {
+  await t.step({
+    ignore: false,
+    name: "should discard keys with undefined values",
+    fn: () => {
+      const expected = {};
+      const actual = deepMapKeys({ prop: undefined });
+
+      assertEquals(actual, expected);
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name: "should transform the keys of nested objects in JsonArrays",
+    fn: () => {
       const jsonArray = [
         {
           item_name: "Apple",
@@ -119,13 +153,150 @@ Deno.test("json.ts/deepMapKeys", async (t) => {
         "foo_bar",
       ]);
     },
-  );
+  });
 
-  await t.step("should throw when passed undefined", () => {
-    try {
-      assertThrows(() => deepMapKeys(undefined), TypeError);
-    } catch (error) {
-      assertEquals(error.message, "Invalid argument type");
-    }
+  await t.step({
+    ignore: false,
+    name: "should throw when passed undefined",
+    fn: () => {
+      try {
+        assertThrows(() => deepMapKeys(undefined), TypeError);
+      } catch (error) {
+        assertEquals(error.message, "Invalid argument type");
+      }
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name: "should omit keys listed in the skip list",
+    fn: () => {
+      const KEY1 = "some-very weird:key";
+      const KEY2 = "2024-07-30T05:13:15.416Z";
+
+      const subject = {
+        first_key: {
+          second_key: {
+            third_key: {
+              [KEY1]: { item_value: 42 },
+            },
+            [KEY2]: { item_value: 43 },
+          },
+        },
+      };
+
+      const skipList = [
+        `first_key.second_key.third_key."${KEY1}"`,
+        `first_key.second_key."${KEY2}"`,
+      ];
+
+      const result = deepMapKeys(subject, camelCase, skipList);
+      assertEquals(result, {
+        firstKey: {
+          secondKey: {
+            thirdKey: {
+              [KEY1]: { itemValue: 42 },
+            },
+            [KEY2]: { itemValue: 43 },
+          },
+        },
+      });
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name: "should omit keys listed in the skip list for JSONArrays",
+    fn: () => {
+      const KEY1 = "some-very weird:key";
+      const KEY2 = "2024-07-30T05:13:15.416Z";
+
+      const subject = [{
+        first_key: {
+          second_key: {
+            third_key: {
+              [KEY1]: { item_value: 42 },
+            },
+            [KEY2]: { item_value: 43 },
+          },
+        },
+      }];
+
+      const skipList = [
+        `[0].first_key.second_key.third_key."${KEY1}"`,
+        `[0].first_key.second_key."${KEY2}"`,
+      ];
+
+      const result = deepMapKeys(subject, camelCase, skipList);
+      assertEquals(result, [{
+        firstKey: {
+          secondKey: {
+            thirdKey: {
+              [KEY1]: { itemValue: 42 },
+            },
+            [KEY2]: { itemValue: 43 },
+          },
+        },
+      }]);
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name:
+      "should process all entries when the skip list contains an empty string",
+    fn: () => {
+      const KEY1 = "some-very weird:key";
+      const KEY2 = "2024-07-30T05:13:15.416Z";
+
+      const subject = [{
+        first_key: {
+          second_key: {
+            third_key: {
+              [KEY1]: { item_value: 42 },
+            },
+            [KEY2]: { item_value: 43 },
+          },
+        },
+      }];
+
+      const skipList = [""];
+
+      const result = deepMapKeys(subject, camelCase, skipList);
+      assertEquals(result, [{
+        firstKey: {
+          secondKey: {
+            thirdKey: {
+              "someVeryWeirdKey": { itemValue: 42 },
+            },
+            "20240730T051315416Z": { itemValue: 43 },
+          },
+        },
+      }]);
+    },
+  });
+
+  await t.step({
+    ignore: false,
+    name: "Should work with the 2nd example from the docs",
+    fn: () => {
+      const subject = [{
+        dates_data: {
+          "2024-07-30T05:00:00.000Z": { item_value: 42 },
+          "2024-08-30T05:02:01.000Z": { item_value: 43 },
+        },
+      }];
+      const skipList = ['[0].dates_data."2024-07-30T05:00:00.000Z"'];
+      const result = deepMapKeys(subject, camelCase, skipList);
+
+      const expected = [{
+        datesData: {
+          "2024-07-30T05:00:00.000Z": { itemValue: 42 },
+          "20240830T050201000Z": { itemValue: 43 },
+        },
+      }];
+
+      assertEquals(result, expected);
+    },
   });
 });
--- packages/json/deep_map_keys.ts
+++ packages/json/deep_map_keys.ts
@@ -1,14 +1,24 @@
 import { isJsonScalar, isPlainObject } from "./type_guards.ts";
 import type { JsonArray, JsonObject, JsonValue } from "./types.ts";
 
+/** @private */
+function escape(s: string): string {
+  const regex = /(\s|\.)/;
+  return regex.test(s) ? `\"${s}\"` : s;
+}
+
+/** @private */
 function idFn<T = unknown>(x: T): T {
   return x;
 }
 
+/** @private */
 function deepMapKeysInternal(
   value: unknown,
   iteratee: (p: string) => string,
-  refs: WeakSet<JsonObject | JsonArray>,
+  skipList: Set<string>,
+  refs: WeakSet<JsonObject | JsonArray> = new WeakSet<JsonObject | JsonArray>(),
+  currentPath: Array<string> = [],
 ): JsonValue {
   if (isJsonScalar(value)) {
     return value;
@@ -21,14 +31,31 @@ function deepMapKeysInternal(
   }
 
   if (Array.isArray(value)) {
-    return value.map((item) => deepMapKeysInternal(item, iteratee, refs));
+    return value.map((item, idx) => {
+      const mapped = deepMapKeysInternal(
+        item,
+        iteratee,
+        skipList,
+        refs,
+        currentPath.concat(`[${idx}]`),
+      );
+      return mapped;
+    }).filter((v) => typeof v !== "undefined");
   }
 
   if (isPlainObject(value)) {
     return Object.entries(value).reduce((accumulator, [k, v]) => {
-      const newKey = iteratee(k);
+      const newKey = skipList.has(currentPath.concat(escape(k)).join("."))
+        ? k
+        : iteratee(k);
       if (v !== undefined) {
-        accumulator[newKey] = deepMapKeysInternal(v, iteratee, refs);
+        accumulator[newKey] = deepMapKeysInternal(
+          v,
+          iteratee,
+          skipList,
+          refs,
+          currentPath.concat(escape(k)),
+        );
       }
 
       return accumulator;
@@ -39,15 +66,24 @@ function deepMapKeysInternal(
 }
 
 /**
- * Returns a *new* a JSON serializable object with the same values as
- * `input` where each own enumerable key of `input` is recursively transformed by
- * `iteratee`. The optional iteratee is invoked with the key name, and the returned
- * value becomes the new key name.
- * The default iteratee is the identity function (this is useful for deep cloning).
+ * Returns a *new* JSON serializable object with the same values as
+ * `input` where each own enumerable key of `input` is recursively
+ * transformed by `iteratee`.
  *
- * @throws If there is a circular reference in `input` or when `input` is not JSON serializable.
+ * @public
  *
- * @example Usage
+ * @param input - A JSON value to process.
+ * @param iteratee - The iteratee is invoked with the key name, and the returned
+ * value becomes the new key name. The default iteratee is the identity
+ * function if it is not provided.
+ * @param skipList - A list of strings representing the path to some key in the
+ * input object. If the key contains a space or a dot ('.') the key must be
+ * enclosed in double or single quotes. For more info see the examples below.
+ *
+ * @throws If there is a circular reference in `input` or when `input` is not
+ * JSON serializable.
+ *
+ * @example
  * ```ts
  * import { deepMapKeys } from "jsr:@y0n1/json/deepMapKeys";
  * import camelCase from "npm:lodash/camelCase.js";
@@ -58,7 +94,7 @@ function deepMapKeysInternal(
  *   address: { street_name: "Main St" },
  * };
  *
- * const result = deepMapKeys(jsonObject, camelCase);
+ * const result = deepMapKeys(jsonObject, camelCase, [street_name]);
  *
  * const expected = {
  *   firstName: "John",
@@ -68,10 +104,31 @@ function deepMapKeysInternal(
  *
  * assertEquals(result, expected);
  * ```
+ * @example Using the skipList parameter
+ * ```ts
+ * const subject = [{
+ *     dates_data: {
+ *       "2024-07-30T05:00:00.000Z": { item_value: 42 },
+ *       "2024-08-30T05:02:01.000Z": { item_value: 43 },
+ *     },
+ *   }];
+ *   const skipList = ['[0].dates_data."2024-07-30T05:00:00.000Z"'];
+ *   const result = deepMapKeys(subject, camelCase, skipList);
+ *
+ *   const expected = [{
+ *     datesData: {
+ *       "2024-07-30T05:00:00.000Z": { itemValue: 42 },
+ *       "20240830T050201000Z": { itemValue: 43 },
+ *     },
+ *   }];
+ *
+ *   assertEquals(result, expected);
+ * ```
  */
 export function deepMapKeys(
   input: unknown,
   iteratee: (p: string) => string = idFn,
+  skipList: Array<string> = [],
 ): JsonValue {
   if (
     input === undefined ||
@@ -84,8 +141,8 @@ export function deepMapKeys(
     throw new TypeError("Invalid argument type");
   }
 
-  const refs = new WeakSet<JsonObject | JsonArray>();
-  const result = deepMapKeysInternal(input, iteratee, refs);
+  const dedupedSkipList = new Set(skipList);
+  const result = deepMapKeysInternal(input, iteratee, dedupedSkipList);
 
   return result;
 }
